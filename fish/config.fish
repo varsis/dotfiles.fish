1cf7fb56f0773d96099db9b3687509f762dcfcbd
Chris Pavlicek <chris@varsisstudio.com>
2022-03-29 09:43:10 +0100

    Update dotfiles

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,11 @@ starship init fish | source
 
 if status is-interactive 
 and not set -q TMUX
-    exec tmux
+    set -g TMUX tmux new-session -d -s base
+    eval $TMUX
+    tmux attach-session -d -t base
 end
+
+fish_vi_key_bindings
+
 clear
--- fish/install.fish
+++ fish/install.fish
@@ -1,2 +1,3 @@
 #!/usr/bin/env fish
 set -U fish_greeting
+set -U FZF_DEFAULT_COMMAND 'rg --files'
